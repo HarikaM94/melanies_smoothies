67107edb875a9a6251bff87cc2543f5ad2df8e96
HarikaM94 <vishnuharika94@gmail.com>
2024-07-29 22:40:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,6 @@ if ingredients_list:
         ingredients_string+= each_fruit+' '
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-#st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Name_on_Order)
             values ('""" + ingredients_string + """','"""+Name_on_Order+"""')"""
